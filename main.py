91a06eabee91b71e155e32aac5185645624db99c
NadavBem <nadav0176@gmail.com>
2024-06-09 19:42:51 +0300

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World, from Nadav Ben menahem :) '
+    return 'Hello World, from Nadav Ben menahem :)'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
